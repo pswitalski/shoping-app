decd19774978e3712b4d47d089e0895dd110cb9b
pawel switalski <pawelswitalski94@gmail.com>
2022-03-11 22:11:12 +0100

    create NavBar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import type { AppProps } from 'next/app';
 import { Global } from '@emotion/react';
 import GlobalStyles from '../styles/GlobalStyles';
+import NavBar from '../components/NavBar/NavBar';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return(
     <>
       <Global styles={GlobalStyles} />
+      <NavBar title='Shopping App' />
       <Component {...pageProps} />
     </>
 
